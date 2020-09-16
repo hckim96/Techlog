158a73eee785fe77b0ff08f37d1623c09127963b
hyeoncheolkim <hyeoncheolkim96@gmail.com>
2020-09-17 03:13:13 +0900

    WIP

--- BackEnd/src/main/java/techLog/controller/MainController.java
+++ BackEnd/src/main/java/techLog/controller/MainController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import techLog.dto.UserDto;
 import techLog.service.UserService;
 
+@Api(tags = {"1. Main"})
+@RequestMapping(value = "/v1")
 @Controller
 @AllArgsConstructor
 public class MainController {
@@ -26,8 +31,9 @@ public class MainController {
     // public String signinPage(){
     //     return "signin success";
     // }
+    @ApiOperation(value = "signup", notes = "signup notes")
     @PostMapping("/api/signup")
-    public String doSignupUser(UserDto userDto){
+    public String doSignupUser(@ApiParam(value = "dto", required = false) UserDto userDto){
         userService.signupUser(userDto);
         return "redirect:/";
     }
